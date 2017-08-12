0db5843f99ba4146d8492e80f5e28a0087d518fe
YetaWF <mikevdm@YetaWF.com>
2017-08-12 12:51:07 -0400

    Revert "gulp"
    This reverts commit 162f49736240e206de29c8840aa8e39fd420908e.

--- post-clone.bat
+++ post-clone.bat
@@ -1,7 +1,6 @@
 Utilities\ProjectSettings\bin\Softelvdm.ProjectSettings.exe SetMVC6 Symlinks
 cd Website
 cmd /c npm install
-cmd /c npm install -g gulp
 cmd /c npm rebuild node-sass
 cmd /c bower install
 cd ..
