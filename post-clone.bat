162f49736240e206de29c8840aa8e39fd420908e
YetaWF <mikevdm@YetaWF.com>
2017-08-12 12:46:52 -0400

    gulp

--- post-clone.bat
+++ post-clone.bat
@@ -1,6 +1,7 @@
 Utilities\ProjectSettings\bin\Softelvdm.ProjectSettings.exe SetMVC6 Symlinks
 cd Website
 cmd /c npm install
+cmd /c npm install -g gulp
 cmd /c npm rebuild node-sass
 cmd /c bower install
 cd ..
