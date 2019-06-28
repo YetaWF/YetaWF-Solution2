0af3661798aa035adf5a67f112ba0492f4c9fd45
YetaWF <mikevdm@YetaWF.com>
2019-06-28 13:36:14 -0400

    post-clone linux

--- post-clone.sh
+++ post-clone.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+# Change ownership of all files/folder
+sudo chown $USER -R .
+# get libgdiplus for webp image support
+sudo apt-get update
+sudo apt-get install -y libgdiplus libc6-dev
+# Build and run ProjectSettings tool to set MVC6 project files and all symlinks
 cp PublicTools/ProjectSettings/ProjectSettings.csproj_MVC6 PublicTools/ProjectSettings/ProjectSettings.csproj
 dotnet build PublicTools/ProjectSettings
 dotnet run -p PublicTools/ProjectSettings SetMVC6
 dotnet run -p PublicTools/ProjectSettings SymLinks
+# Build the website
 cd Website
 npm install
-bower install
+npm rebuild cwebp-bin
+sudo bower install --allow-root
 dotnet build
+# Run the website, connect with browser at http://localhost:50000
 dotnet run
 cd ..
-
 
